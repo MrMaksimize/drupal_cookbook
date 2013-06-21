b35a0ab8ce6d8b45a7ed82c1da8fd9cdad1c4a1f
Maksim Pecherskiy <max@maksimize.com>
2013-06-21 09:12:00 -0500

    Removing commented junk.

--- recipes/kw_foo.rb
+++ recipes/kw_foo.rb
@@ -1,10 +1,3 @@
-# git '/var/drupals/foo' do
-#   repository  'http://git.drupal.org/project/drupal.git'
-#   reference   '8.x'
-#   action      :sync
-#   user        'vagrant'
-#   group       'www-data'
-# end
 
 drupal_site 'kw' do
   root        '/var/drupals/kw'
